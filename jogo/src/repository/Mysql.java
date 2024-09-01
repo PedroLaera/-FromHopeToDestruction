e889f65032947bbe6e70a2af02a4c9125353c4eb
Pedro Henrique Laera Ferreira de Castro <161781371+PedroLaera@users.noreply.github.com>
2024-09-01 20:09:21 -0300

    java
    finalização da conexão com o banco de dados, partindo para o modelo das tabelas

--- jogo/src/repository/Mysql.java
+++ jogo/src/repository/Mysql.java
@@ -1,4 +1,28 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+
+import static java.lang.Class.forName;
+
 public class Mysql {
+    public static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/jogo",
+                    "root",
+                    ""
+            );
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Erro ao tentar conectar com o banco de dados...");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Erro ao tentar importar o driver do banco de dados...");
+        } return null;
+    }
 }
