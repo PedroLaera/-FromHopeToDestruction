47f55ff334cca50a1b25c8f2afeafbf24427aa56
Pedro Henrique Laera Ferreira de Castro <161781371+PedroLaera@users.noreply.github.com>
2024-09-01 22:49:37 -0300

    java
    conclusão da conexão das cenas com o banco de dados

---
+++ jogo/src/repository/CenaDAO.java
@@ -0,0 +1,27 @@
+package repository;
+
+import model.Cena;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CenaDAO {
+    public static Cena findCenaById(Integer id) throws SQLException {
+        Connection conn = Mysql.getConnection();
+        String sql = "select * from cenas where id_cena = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+        Cena cena = new Cena();
+
+
+        if (rs.next()){
+            cena.setIdCena(rs.getInt("id_cena"));
+            cena.setDescricao(rs.getString("descricao"));
+            cena.setItens();
+        }
+        return cena;
+    }
+}
---
+++ jogo/src/repository/ItemDAO.java
@@ -0,0 +1,12 @@
+package repository;
+
+import model.Item;
+
+public class ItemDAO {
+    public static Item findItemById(Integer id) {
+
+        //add os itens do jogo
+        
+        return new Item();
+    }
+}
