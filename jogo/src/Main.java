199f765611318473c01d1bfe47c85ca69e8f3fc2
Pedro Henrique Laera Ferreira de Castro <161781371+PedroLaera@users.noreply.github.com>
2024-09-05 03:37:11 -0300

    Java
    Adicionando detalhes adicionais as classes do ItemDAO

--- jogo/src/Main.java
+++ jogo/src/Main.java
@@ -1,14 +1,20 @@
 import model.Cena;
+import model.Item;
 import repository.CenaDAO;
+import repository.ItemDAO;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Cena cena = CenaDAO.findCenaById(1);
-
             System.out.println(cena.toString());
+
+
+            List<Item> itens = ItemDAO.findItensByScene(cena);
+            System.out.println("Itens: " + itens);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- jogo/src/repository/ItemDAO.java
+++ jogo/src/repository/ItemDAO.java
@@ -1,12 +1,39 @@
 package repository;
 
+import model.Cena;
 import model.Item;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemDAO {
     public static Item findItemById(Integer id) {
-
-        //add os itens do jogo
-        
         return new Item();
+    }
+    public static List<Item> findItensByScene (Cena cena) throws SQLException {
+        Connection connection = Mysql.getConnection();
+        String sql = "select * from itens i where id_cena_atual = ?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setInt(1, cena.getIdCena());
+        ResultSet resultSet = ps.executeQuery();
+
+        List<Item> itens = new ArrayList<>();
+        while (resultSet.next()){
+            Item item = new Item();
+            item.setIdItem(resultSet.getInt("id_item"));
+            item.setNome(resultSet.getString("nome"));
+
+            Integer idCenaAtual = resultSet.getInt("id_cena_atual");
+            Cena cenaAtual = CenaDAO.findCenaById(idCenaAtual);
+
+            item.setCenaAtual(cenaAtual);
+
+            itens.add(item);
+        }
+        return itens;
     }
 }
