887150b94032aab4b88882511e45abfaad9f3b72
Pedro Henrique Laera Ferreira de Castro <161781371+PedroLaera@users.noreply.github.com>
2024-09-05 22:39:53 -0300

    Java
    Adicionando Spark para pesquisa

--- jogo/src/Main.java
+++ jogo/src/Main.java
@@ -1,13 +1,19 @@
+import com.google.gson.Gson;
 import model.Cena;
 import model.Item;
 import repository.CenaDAO;
 import repository.ItemDAO;
+import spark.Spark;
 
 import java.sql.SQLException;
 import java.util.List;
 
+import static spark.Spark.port;
+
 public class Main {
     public static void main(String[] args) {
+        Gson gson = new Gson();
+        port(3000);
         try {
             Cena cena = CenaDAO.findCenaById(1);
             System.out.println(cena.toString());
@@ -15,8 +21,12 @@ public class Main {
 
             List<Item> itens = ItemDAO.findItensByScene(cena);
             System.out.println("Itens: " + itens);
+
+            Spark.get("/cena", (req,res) -> gson.toJson(itens));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+
     }
 }
