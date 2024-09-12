091dab887154efdedaaa9700ddfac7ada441cf5c
Pedro Henrique Laera Ferreira de Castro <161781371+PedroLaera@users.noreply.github.com>
2024-09-12 02:04:45 -0300

    Java
    Adicionando Class de Comandos para adicionar funcionalidades ao mesmo

--- jogo/src/Main.java
+++ jogo/src/Main.java
@@ -15,12 +15,14 @@ import static spark.Spark.port;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        Save save = SaveDAO.newGame();
-        System.out.println(save.getCenaAtual().getDescricao());
+//        Save save = SaveDAO.newGame();
+//        System.out.println(save.getCenaAtual().getDescricao());
 
         //Assistir aula do Jucelino do dia 10/09 aula 10
         //Implementar funcionalidade de cada comando
 
+//        Cena cena = CenaDAO.findCenaById(1);
+//        System.out.println(cena);
 
 
 
@@ -30,20 +32,20 @@ public class Main {
 
 
          //ligação com insomnia
-//        Gson gson = new Gson();
-//        port(3000);
-//        try {
-//            Cena cena = CenaDAO.findCenaById(1);
-//            System.out.println(cena.toString());
-//
-//
-//            List<Item> itens = ItemDAO.findItensByScene(cena);
-//            System.out.println("Itens: " + itens);
-//
-//            Spark.get("/cena", (req,res) -> gson.toJson(itens));
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
+        Gson gson = new Gson();
+        port(3000);
+        try {
+            Cena cena = CenaDAO.findCenaById(3);
+            System.out.println(cena.toString());
+
+
+            List<Item> itens = ItemDAO.findItensByScene(cena);
+            System.out.println("Itens: " + itens);
+
+            Spark.get("/cena", (req,res) -> gson.toJson(itens));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
